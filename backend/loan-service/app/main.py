0a03af6f530443a65fdb184fd81be76036f24cda
VishnuRathore98 <rathorevishnu666@gmail.com>
2025-12-26 22:45:24 +0530

    added lifespan for startup

--- backend/loan-service/app/main.py
+++ backend/loan-service/app/main.py
@@ -1,11 +1,30 @@
 from fastapi import FastAPI
 from app.api.v1 import loan_routes
 from app.core.database import Base, engine
+from contextlib import asynccontextmanager
+from app.core.messages import start_credit_decision_consumer
+import threading
+import logging
 
-app = FastAPI(title="Loan Service")
+decision_logger = logging.get_logger("loan-decision-consumer")
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    threading.Thread(
+        target=start_credit_decision_consumer,
+        daemon=True,
+    ).start()
+    
+    yield
+
+    # put in closing thread logic
+
+
+app = FastAPI(title="Loan Service", lifespan=lifespan)
 
 
 Base.metadata.create_all(bind=engine)
+
 
 
 @app.get("/health")
